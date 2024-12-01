8624f8f574eb93b86f4a83303f6ae73ac42bdd62
Michael Aliendro <mgbaliendro@gmail.com>
2024-12-01 15:41:49 -0300

    replace reduce for sum method for iterators

--- day1/src/part02/mod.rs
+++ day1/src/part02/mod.rs
@@ -1,7 +1,7 @@
 use std::collections::HashMap;
 
 pub fn run(contents: &str) -> u32 {
-    let mut left_map: HashMap<u32, u32> = HashMap::new();
+    let mut left_list: Vec<u32> = Vec::new();
     let mut right_map: HashMap<u32, u32> = HashMap::new();
 
     for line in contents.lines() {
@@ -10,20 +10,18 @@ pub fn run(contents: &str) -> u32 {
             .filter(|v| *v != "")
             .map(|v| v.parse::<u32>().unwrap());
         let (left, right) = (parsed.next().unwrap(), parsed.next().unwrap());
-
-        *left_map.entry(left).or_default() += 1;
+        left_list.push(left);
         *right_map.entry(right).or_default() += 1;
     }
 
-    left_map
-        .into_iter()
-        .map(|(k, _v)| {
+    left_list
+        .iter()
+        .map(|k| {
             if let Some(count) = right_map.get(&k) {
                 k * count
             } else {
                 0
             }
         })
-        .reduce(|a, b| a + b)
-        .unwrap()
+        .sum()
 }
