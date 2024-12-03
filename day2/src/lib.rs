2faf8b0febe76da4a5469c80469946e5c538972f
Michael Aliendro <mgbaliendro@gmail.com>
2024-12-03 11:43:48 -0300

    day2 part 01 solution

---
+++ day2/src/lib.rs
@@ -0,0 +1,34 @@
+fn parse_line(line: &str) -> Vec<u32> {
+    line.split_whitespace()
+        .map(|level| level.parse::<u32>().expect("Failed to parse level"))
+        .collect::<Vec<_>>()
+}
+
+fn check_safety(line: &Vec<u32>) -> bool {
+    let (first, second) = (line[0], line[1]);
+    let initial_direction = first < second;
+
+    for (a, b) in line.iter().zip(line.iter().skip(1)) {
+        let current_direction = a < b;
+
+        if initial_direction != current_direction {
+            return false;
+        }
+
+        let height = a.abs_diff(*b);
+
+        if height < 1 || height > 3 {
+            return false;
+        }
+    }
+
+    true
+}
+
+pub fn part_one(content: &str) -> usize {
+    content.lines().map(parse_line).filter(check_safety).count()
+}
+
+pub fn part_two(_content: &str) -> usize {
+    todo!()
+}
--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,3 +1,13 @@
+use day2::{part_one, part_two};
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let content = fs::read_to_string("input/day2/input").unwrap();
+
+    let solution1 = part_one(&content);
+
+    // let solution2 = part_two(&content);
+
+    println!("{solution1:?}");
+    // println!("{solution2:?}");
 }
