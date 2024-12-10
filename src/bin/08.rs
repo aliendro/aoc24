c5290d745bfe4232a9fc2e07ee41573a9fdb6e48
Michael Aliendro <mgbaliendro@gmail.com>
2024-12-10 13:27:54 -0300

    scaffold day 8

---
+++ src/bin/08.rs
@@ -0,0 +1,13 @@
+use std::fs;
+
+fn main() {
+    let content = fs::read_to_string("data/inputs/08").expect("Failed to read from input file");
+
+    let part01_solution = "";
+
+    println!("Part01 solution: {part01_solution}");
+
+    let part02_solution = "";
+
+    println!("Part02 solution: {part02_solution}");
+}
