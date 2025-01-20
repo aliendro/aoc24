703247a90c3423ca169de053c2e8ddb9f9a6e1f0
Michael Aliendro <mgbaliendro@gmail.com>
2025-01-20 11:33:55 -0300

    add bash script to pull inputs and puzzles

---
+++ script.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+for i in $(seq 1 25); do
+	if [[ $i -ge 0 && $i -le 9 ]]; then
+		filename="0$i"
+	else
+		filename="$i"
+	fi
+	aoc download -d $i -i data/inputs/$filename -p data/puzzles/$filename.md
+done
